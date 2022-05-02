ffcb63ac7992919b3805a70d908e8bcb6af8f0e0
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-05-02 17:20:46 +0300

    lesson 2 task 1 fix actions 4

--- lesson-2/task/src/index.js
+++ lesson-2/task/src/index.js
@@ -1,14 +1,10 @@
 import { createStore } from 'redux';
 
-export const increment = {
-  type: 'COUNTER/INCREMENT',
-};
+export const increment = () => ({ type: 'COUNTER/INCREMENT' });
 
-export const decrement = {
-  type: 'COUNTER/DECREMENT',
-};
+export const decrement = () => ({ type: 'COUNTER/DECREMENT' });
 
-export const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     default:
       return state;
