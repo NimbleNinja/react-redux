850e061c7ca6f5028de7e5a1548fd31189a414c7
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-05-04 20:07:12 +0300

    lesson 4 hw 1 fix  isNextPageAvailable v4

--- lesson-4/hw-1/src/users/Pagination.jsx
+++ lesson-4/hw-1/src/users/Pagination.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const Pagination = ({ goNext, goPrev, currentPage, totalItems, itemsPerPage }) => {
-  const isPrevPageAvailable = currentPage === 1;
+  const isPrevPageAvailable = currentPage > 1;
+
   const isNextPageAvailable =
-    Math.ceil(totalItems / itemsPerPage) === currentPage || totalItems === 0;
+    Math.ceil(totalItems / itemsPerPage) > currentPage || totalItems === 0;
 
   return (
     <div className="pagination">
-      <button disabled={isPrevPageAvailable} onClick={goPrev} className="btn">
-        {isPrevPageAvailable ? '' : '←'}
+      <button disabled={!isPrevPageAvailable} onClick={goPrev} className="btn">
+        {isPrevPageAvailable ? '←' : ''}
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button disabled={isNextPageAvailable} onClick={goNext} className="btn">
-        {isNextPageAvailable ? '' : '→'}
+      <button disabled={!isNextPageAvailable} onClick={goNext} className="btn">
+        {isNextPageAvailable ? '→' : ''}
       </button>
     </div>
   );
