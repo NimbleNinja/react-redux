ef1d266c485610a2b78450a459b7412a2a63329d
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-05-04 18:55:37 +0300

    lesson 4 hw 1

---
+++ lesson-4/hw-1/src/users/Pagination.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Pagination = ({ goNext, goPrev, currentPage, totalItems, itemsPerPage }) => {
+  const isPrevPageAvailable = currentPage === 1;
+
+  const isNextPageAvailable = Math.ceil(totalItems / itemsPerPage) === currentPage;
+  return (
+    <div className="pagination">
+      <button disabled={isPrevPageAvailable} onClick={goPrev} className="btn">
+        {isPrevPageAvailable ? '' : '←'}
+      </button>
+      <span className="pagination__page">{currentPage}</span>
+      <button disabled={isNextPageAvailable} onClick={goNext} className="btn">
+        {isNextPageAvailable ? '' : '→'}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
---
+++ lesson-4/hw-1/src/users/UsersList.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Pagination from './Pagination';
+import User from './User';
+import * as paginationActions from './pagination.actions';
+
+class UsersList extends Component {
+  render() {
+    const { goNext, goPrev, users, currentPage } = this.props;
+
+    const itemsPerPage = 3;
+    const newCurrentPage = currentPage + 1;
+
+    const sliceFrom = itemsPerPage * currentPage;
+    const sliceTo = itemsPerPage * newCurrentPage;
+
+    const usersToRender = users.slice(sliceFrom, sliceTo);
+    return (
+      <div>
+        <Pagination
+          goNext={goNext}
+          goPrev={goPrev}
+          currentPage={newCurrentPage}
+          totalItems={users.length}
+          itemsPerPage={itemsPerPage}
+        />
+        <ul className="users">
+          {usersToRender.map(({ id, name, age }) => (
+            <User key={id} name={name} age={age} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapState = state => ({
+  users: state.users.usersList,
+  currentPage: state.users.currentPage,
+});
+
+const mapDispatch = {
+  goNext: paginationActions.goNext,
+  goPrev: paginationActions.goPrev,
+};
+
+export default connect(mapState, mapDispatch)(UsersList);
