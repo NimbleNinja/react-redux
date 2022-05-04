5cbd1714fd071ff134c277569cd1a0b68a787ffe
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-05-04 13:44:22 +0300

    lesson 4 task 1 redux old

--- lesson-4/task-1/src/Counter.jsx
+++ lesson-4/task-1/src/Counter.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as counterActions from './counter.actions';
 
 class Counter extends Component {
   render() {
+    const { counter, increment, decrement, reset } = this.props;
     return (
       <div className="counter">
-        <button className="counter__button">-</button>
-        <span className="counter__value">17</span>
-        <button className="counter__button">+</button>
+        <button onClick={decrement} className="counter__button">
+          -
+        </button>
+        <span onClick={reset} className="counter__value">
+          {counter}
+        </span>
+        <button onClick={increment} className="counter__button">
+          +
+        </button>
       </div>
     );
   }
 }
 
-export default Counter;
+const mapState = state => ({
+  counter: state,
+});
+
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+
+export default connect(mapState, mapDispatch)(Counter);
