2c43647b3c502fafc5869b8c7869f505060f505a
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-05-07 15:37:36 +0300

    lesson 6 hw 1 redux v1 second check

--- lesson-6/hw-1/src/weather/Weather.jsx
+++ lesson-6/hw-1/src/weather/Weather.jsx
@@ -1,39 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import weatherDataSelector from './weather.selectors';
 import * as weatherActions from './weather.actions';
 
-const Weather = ({ towns, getWeatherData }) => {
-  useEffect(() => {
-    getWeatherData();
-  }, []);
+class Weather extends Component {
+  componentDidMount() {
+    this.props.getWeatherData();
+  }
 
-  return (
-    <main className="weather">
-      <h1 className="weather__title">Weather data</h1>
-      <ul className="cities-list">
-        {towns.map(({ id, name, temperature }) => (
-          <li key={id} className="city">
-            <span className="city__name">{name}</span>
-            <span className="city__temperature">{`${temperature} F`}</span>
+  render() {
+    return (
+      <main className="weather">
+        <h1 className="weather__title">Weather data</h1>
+        <ul className="cities-list">
+          {this.props.towns.map(({ id, name, temperature }) => (
+            <li key={id} className="city">
+              <span className="city__name">{name}</span>
+              <span className="city__temperature">{`${temperature} F`}</span>
+            </li>
+          ))}
+          <li className="city">
+            <span className="city__name">Lake Hilmaside</span>
+            <span className="city__temperature">78 F</span>
           </li>
-        ))}
-        <li className="city">
-          <span className="city__name">Lake Hilmaside</span>
-          <span className="city__temperature">78 F</span>
-        </li>
-        <li className="city">
-          <span className="city__name">Lake Minnieberg</span>
-          <span className="city__temperature">8 F</span>
-        </li>
-        <li className="city">
-          <span className="city__name">East Gerhardshire</span>
-          <span className="city__temperature">23 F</span>
-        </li>
-      </ul>
-    </main>
-  );
-};
+          <li className="city">
+            <span className="city__name">Lake Minnieberg</span>
+            <span className="city__temperature">8 F</span>
+          </li>
+          <li className="city">
+            <span className="city__name">East Gerhardshire</span>
+            <span className="city__temperature">23 F</span>
+          </li>
+        </ul>
+      </main>
+    );
+  }
+}
 
 const mapState = state => ({
   towns: weatherDataSelector(state),
